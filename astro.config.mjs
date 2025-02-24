0fd1e0fdda1b73268a5798e87456668803f8366c
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-02-24 21:51:47 +0800

    1st day

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+	integrations: [react(), icon()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
